7fcf9b8303533aa62477c39cf403190e138f8fd0
Travis Webb <me@traviswebb.com>
2015-07-12 18:32:18 -0400

    define base models

---
+++ api/controllers/LocationController.js
@@ -0,0 +1,11 @@
+/**
+ * LocationController
+ *
+ * @description :: Server-side logic for managing Locations
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/OrganizationController.js
@@ -0,0 +1,11 @@
+/**
+ * OrganizationController
+ *
+ * @description :: Server-side logic for managing Organizations
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
